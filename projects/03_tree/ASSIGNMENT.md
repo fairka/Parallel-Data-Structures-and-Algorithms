d47a1ee28ea443ef7961ab57a4931de41853b480
Kaden Fairchild <kaden.fairchild@wallawalla.edu>
2021-06-03 08:01:16 +0000

    proj 3 first test

--- projects/03_tree/ASSIGNMENT.md
+++ projects/03_tree/ASSIGNMENT.md
@@ -2,15 +2,15 @@
 
 ## Walla Walla University -- Department of Computer Science
 
-_Name:_ ?
+_Name:_ Kaden Fairchild
 
 _Course:_ CPTR242
 
-_Assignment:_ ?
+_Assignment:_ Proj 3
 
-_Date:_ ?
+_Date:_ 6/8/2021
 
-_Estimated:_ ?
+_Estimated:_ 5 hr
 
 _Actual Time:_ ?
 
@@ -23,6 +23,6 @@ I hereby certify that the code included in this assignment is ENTIRELY my own or
 
 ---
 
-_Digital Signature:_ ???
+_Digital Signature:_ Kaden Fairchild
 
 (Type name or include signature image)
--- projects/03_tree/UsgsTree.cpp
+++ projects/03_tree/UsgsTree.cpp
@@ -26,7 +26,14 @@ using namespace bridges;
 EarthquakeUSGS
 UsgsTree::getLargestQuake(BSTElement<float, EarthquakeUSGS> *root,
                           std::string color) {
-  return nullptr;
+  if (root == nullptr) {
+    return 0;
+  } else if (root->getRight() != nullptr) {
+    return getLargestQuake(root->getRight(), color);
+  } else {
+    root->getVisualizer()->setColor(color);
+    return root->getValue();
+  }
 }
 
 // Function: Finds the largest quake magnitude in a BST.
@@ -35,16 +42,41 @@ UsgsTree::getLargestQuake(BSTElement<float, EarthquakeUSGS> *root,
 EarthquakeUSGS
 UsgsTree::getSmallestQuake(BSTElement<float, EarthquakeUSGS> *root,
                            std::string color) {
-  return nullptr;
+  if (root == nullptr) {
+    return 0;
+  } else if (root->getLeft() != nullptr) {
+    return getSmallestQuake(root->getLeft(), color);
+  } else {
+    root->getVisualizer()->setColor(color);
+    return root->getValue();
+  }
 }
 
 // Function: Finds all nodes in the BST that are between the min and max
-// parameters. 
-// Pre:  BST has been initialized and populated. 
+// parameters.
+// Pre:  BST has been initialized and populated.
 // Post: Returns the number of quakes in this range and visualizes them.
+
 int UsgsTree::countRange(float min, float max,
                          BSTElement<float, EarthquakeUSGS> *root,
                          std::string color) {
+  // if(root == nullptr){
+  //     return 0;
+  // }
+
+  // if(root->getValue() == min && root->getValue() == max){
+  //     root->getVisualizer()->setColor(color);
+  //     return 1;
+  // }
+  // if(root->getValue() <= max && root->getValue() >= min){
+  //     root->getVisualizer()->setColor(color);
+  //     return 1 + countRange(min, max, root->getLeft(), color) +
+  //                 countRange(min, max, root->getRight(),color);
+  // } else if(root->getValue() < min){
+  //     return countRange(min, max, root->getRight(), color);
+  // } else{
+  //     return countRange(min, max, root->getLeft(), color);
+  // }
   return 0;
 }
 
@@ -68,4 +100,14 @@ int UsgsTree::countWithStyle(BSTElement<float, EarthquakeUSGS> *root,
 // Function: Resets the BST visualization back to the defaults.
 // Pre:  BST has been initialized and populated.
 // Post: The BST has no visualizations.
-void UsgsTree::resetVisualization(BSTElement<float, EarthquakeUSGS> *root) {}
+void UsgsTree::resetVisualization(BSTElement<float, EarthquakeUSGS> *root) {
+
+  if (root->getLeft() != nullptr) {
+    root->getLeft()->getVisualizer()->setColor(("#4682b4"));
+    resetVisualization(root->getLeft());
+  }
+  if (root->getRight() != nullptr) {
+    root->getRight()->getVisualizer()->setColor(("#4682b4"));
+    resetVisualization(root->getRight());
+  }
+}
